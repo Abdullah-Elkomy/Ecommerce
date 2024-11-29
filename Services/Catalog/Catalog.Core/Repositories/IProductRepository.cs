d70d337275d67cedfcb25c32ed73f27b6bf5ba90
Abdullah <abdullahelkomy95@gmail.com>
2024-11-29 18:49:23 +0200

    Final touches in CatalogApi Microservice with MediatR pattern and Specification pagination pattern

--- Services/Catalog/Catalog.Core/Repositories/IProductRepository.cs
+++ Services/Catalog/Catalog.Core/Repositories/IProductRepository.cs
@@ -1,16 +1,16 @@
 ﻿using Catalog.Core.Entities;
+using Catalog.Core.Specs;
 
 namespace Catalog.Core.Repositories
 {
     public interface IProductRepository
     {
-        Task<IEnumerable<Product>> GetAllProducts();
+        Task<Pagination<Product>> GetAllProducts(CatalogSpecParams catalogSpecParams);
         Task<Product> GetProductById(string id);
         Task<IEnumerable<Product>> GetProductsByName(string productName);
         Task<IEnumerable<Product>> GetProductsByBrand(string brandName);
         Task<Product> CreateProduct(Product product);
         Task<bool> UpdateProduct(Product product);
         Task<bool> DeleteProduct(string id);
-
     }
 }
