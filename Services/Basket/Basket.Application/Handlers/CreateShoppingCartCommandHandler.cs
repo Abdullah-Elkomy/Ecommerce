a1e75e587413216eb76470bc3ee61170887cacd4
Abdullah <abdullahelkomy95@gmail.com>
2024-12-02 17:54:28 +0200

    consuming done

--- Services/Basket/Basket.Application/Handlers/CreateShoppingCartCommandHandler.cs
+++ Services/Basket/Basket.Application/Handlers/CreateShoppingCartCommandHandler.cs
@@ -1,4 +1,5 @@
 ﻿using Basket.Application.Commands;
+using Basket.Application.GrpcServices;
 using Basket.Application.Mappers;
 using Basket.Application.Responses;
 using Basket.Core.Entities;
@@ -10,14 +11,20 @@ namespace Basket.Application.Handlers
     public class CreateShoppingCartCommandHandler : IRequestHandler<CreateShoppingCartCommand, ShoppingCartResponse>
     {
         private readonly IBasketRepository _basketRepository;
+        private readonly DiscountGrpcService _discountGrpcService;
 
-        public CreateShoppingCartCommandHandler(IBasketRepository basketRepository)
+        public CreateShoppingCartCommandHandler(IBasketRepository basketRepository, DiscountGrpcService discountGrpcService)
         {
             _basketRepository = basketRepository;
+            _discountGrpcService = discountGrpcService;
         }
         public async Task<ShoppingCartResponse> Handle(CreateShoppingCartCommand request, CancellationToken cancellationToken)
         {
-            //TODO : Will be integrated Discount service
+            foreach (var item in request.Items)
+            {
+                var coupon = await _discountGrpcService.GetDiscount(item.ProductName);
+                item.Price -= coupon.Amount;
+            }
             var shoppingCard = await _basketRepository.UpdateBasket(new ShoppingCart { UserName = request.UserName, Items = request.Items });
             var shoppingCardResponse = BasketMapper.Mapper.Map<ShoppingCartResponse>(shoppingCard);
             return shoppingCardResponse;
